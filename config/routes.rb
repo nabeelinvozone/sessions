852ea851184f154f40c379c0f72c561ed5af5b0e
Nabeel Bakhash <nabeelbakhash@MP17-NabeelB-SSE-428.local>
2022-01-13 15:48:10 +0500

    changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # get 'articles', to: "articles#index"
   # get 'articles/new'
-  get 'articles/show', as: "article_2"
-  # resources :articles
+  # get 'articles/show', as: "article_2"
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
